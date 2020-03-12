d3a44679ddba8bb6331f62ce2928e25045ff694e
Taylor J Saintable <tsaintable@gmail.com>
2020-03-12 18:44:33 -0200

    Sorted array

--- MiniMaxSum.py
+++ MiniMaxSum.py
@@ -2,11 +2,12 @@ def miniMaxSum(arr):
     arrsum = 0
     minimum = 0
     maximum = 0
+    arr.sort()
     for index in range(len(arr)):
         arrsum += arr[index]
         minimum = arrsum - arr[index]
         maximum = arrsum - arr[len(arr)-1-index]
     return minimum,maximum
 
-myarr = [1,2,3,4,5]
+myarr = [1,3,4,5,6]
 print(miniMaxSum(myarr))
