043e99be1c96c13467040a2b636e796b3910aab0
Taylor J Saintable <tsaintable@gmail.com>
2020-03-21 05:34:01 -0200

    Moved files

---
+++ CuboidCombination.py
@@ -0,0 +1,14 @@
+def CuboidCombination(X,Y,Z,N):
+    ar = [] 
+    p = 0 
+    for i in range ( X + 1 ):
+        for j in range( Y + 1):
+            for k in range(Z + 1):
+                if i+j+k != N:
+                    ar.append([]) 
+                    ar[p] = [ i , j , k ] 
+                    p+=1 
+    return ar
+
+print(CuboidCombination(1,1,1,2))
+print(CuboidCombination(2,2,2,2))
