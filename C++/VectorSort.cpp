7be501a7099c0dbeb3cd0cb0a05ae7cb8b1586c5
Taylor J Saintable <tsaintable@gmail.com>
2020-05-04 17:14:02 -0400

    .

--- C++/VectorSort.cpp
+++ C++/VectorSort.cpp
@@ -3,28 +3,31 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
 vector<int> vec;
 
-int main()
-{
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
 
     int input = 0;
     int n;
     cin >> n;
-
     for (int i = 0; i < n; i++)
     {
         cin >> input;
         vec.push_back(input);
     }
     sort(vec.begin(), vec.end());
-    for (int i = 0; i < n; i++)
-    {
-        cout << vec[i] << " ";
+    int len;
+    int begin,end;
+    int s = sizeof(vec)/sizeof(vec[0]);
+    cin >> begin >> end;
+    for (int index = 0; index < s; index++){
+        
     }
-
+    cout << s;
+    erase(begin);
     return 0;
 }
